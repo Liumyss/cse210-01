e8d9db3e1daa970b40d4c8b5115b4691732b9ebb
Liumyss <computer.46kmp@simplelogin.fr>
2022-04-28 11:08:38 -0600

    commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/* 
+
+    Project Name: cse210_01 TicTacToe
+
+*/
+
+using System;
+
+namespace cse210_01
+{
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello People!");
+
+            Console.WriteLine("Yes");
+        }
+    }
+}
